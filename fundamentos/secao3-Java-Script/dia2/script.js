142d00746508e1d4894d2a15932df65ac7fa6e78
Douglas-Carvalho-Aguiar <douglas.sw228@gmail.com>
2023-05-05 01:06:59 -0300

    resolução dos exercicios de Array e loop for

--- fundamentos/secao3-Java-Script/dia2/script.js
+++ fundamentos/secao3-Java-Script/dia2/script.js
@@ -1,5 +1,26 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let contador =0;
+let result = 0;
+let maiorNumero = 0;
+let contador2;
+let maior =0;
+let criandoArreys = [];
+
 
 for(let index = 0; index <numbers.length; index+=1){
     console.log(numbers[index]);
+    soma = soma + numbers[index];
+    contador = contador +=1;
+
+
+}
+result = soma/contador;
+console.log(soma);
+console.log(result);
+console.log(contador2);
+if(result >20){
+    console.log('O valor da média aritmética é maior que 20');
+}else{
+    console.log('O valor da média aritmética é menor ou igual a 20”');
 }
